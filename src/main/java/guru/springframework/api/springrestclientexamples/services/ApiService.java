7f9f1e4e25e9ce6a1efd4e7784da0212b04bc706
AdRusinek <rusinekadrian19@gmail.com>
2019-02-19 20:29:10 +0100

    Added first REST template to return JSON and bind it to properties by name

---
+++ src/main/java/guru/springframework/api/springrestclientexamples/services/ApiService.java
@@ -0,0 +1,10 @@
+package guru.springframework.api.springrestclientexamples.services;
+
+import guru.springframework.api.domain.User;
+
+import java.util.List;
+
+public interface ApiService {
+
+    List<User> getUsers(Integer limit);
+}
